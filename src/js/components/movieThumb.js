e138a7614526ba17708ebadcfbef681888a9db2c
Matt Coleman <hello@matt-coleman.co.uk>
2016-09-12 18:00:08 +0100

    Added Poster image under title

--- src/js/components/movieThumb.js
+++ src/js/components/movieThumb.js
@@ -3,11 +3,13 @@ import React from 'react';
 module.exports = React.createClass({
   displayName: 'Movies',
   render: function() {
-  	var movie = this.props.movie;
-
+    var movie = this.props.movie;
+    var image = movie.Poster;
+    
     return (
       <div className="movie">
-      	<h2>{movie.Title}</h2>
+        <h2>{movie.Title}</h2>
+        <img src={movie.Poster} alt={movie.Title} width="250" />
       </div>
     );
   }
