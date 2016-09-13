ea344f25dc8d356eeda19b64007e7f50206232f2
Matt Coleman <hello@matt-coleman.co.uk>
2016-09-13 12:30:26 +0100

    Added if statement to only show show movie posters when there is one to display

--- src/js/components/movieThumb.js
+++ src/js/components/movieThumb.js
@@ -3,13 +3,12 @@ import React from 'react';
 module.exports = React.createClass({
   displayName: 'Movies',
   render: function() {
-    var movie = this.props.movie;
-    var image = movie.Poster;
-    
+  	var movie = this.props.movie;
+    var image = (movie.Poster && movie.Poster.search('http') > -1) ? (<img src={movie.Poster} alt={movie.Title} className="movie-poster" />) : (<div className="coming-soon" />)
     return (
       <div className="movie">
         <h2>{movie.Title}</h2>
-        <img src={movie.Poster} alt={movie.Title} width="250" />
+        {image}
       </div>
     );
   }
